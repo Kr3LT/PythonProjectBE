27102637948b27809e3416075aa866be24b173d0
Kim Dung <ngothikimdung3012@gmail.com>
2022-12-12 20:54:04 +0700

    LoaiSanPham/KhachHang

---
+++ app/Service/KhachHangService.py
@@ -0,0 +1,13 @@
+from app.Repository import KhachHangRepository
+
+def createKhachHang(MaKhachHang, TenKhachHang, SoDienThoai, DiaChi):
+    return KhachHangRepository.createKhachHang(MaKhachHang, TenKhachHang, SoDienThoai, DiaChi)
+
+def getAllKhachHang():
+    return KhachHangRepository.getAllKhachHang()
+
+def updateKhachHang(MaKhachHang, TenKhachHang, SoDienThoai, DiaChi):
+    return KhachHangRepository.updateKhachHang(MaKhachHang, TenKhachHang, SoDienThoai, DiaChi)
+
+def deleteKhachHang(MaKhachHang):
+    return KhachHangRepository.deleteKhachHang(MaKhachHang)
